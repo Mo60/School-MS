6301c06e9ca2819dfd58c7345c34c2f960d0ed8c
Mo Khalil <mkhalil@cougarnet.uh.edu>
2022-10-21 16:51:24 -0500

    added easy way to build where website is

--- frondend/vite.config.js
+++ frondend/vite.config.js
@@ -3,5 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+   //when deoplying 
+   // build: {
+   //    outDir: 'C:/dist',
+   //    emptyOutDir: true
+   // },
    plugins: [vue()]
 })
