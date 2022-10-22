2f4d4adfd67c424a7a5c922942416d7252bc1adc
Mo Khalil <mkhalil@Central.UH.EDU>
2022-10-22 00:18:05 -0500

    nav bar

--- frondend/src/App.vue
+++ frondend/src/App.vue
@@ -1,14 +1,6 @@
-<script>
-import pages from '~pages'
-</script>
 <template>
-    <nav class="text-center">
-
-     <p v-for="component in pages" :key="pages.component">
-                  {{ pages.component}}egrgrgr
-     </p>
-     <p>eee {{pages}}  eee</p> 
-    <router-link to="/">Home</router-link> | <router-link to="/about">About</router-link> |
+   <nav class="text-center">
+   <router-link to="/">Home</router-link> | <router-link to="/about">About</router-link> |
     <router-link to="/contact">Contact</router-link>
   </nav>
   <main>
