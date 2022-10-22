5c95de79457aa034a25aea3c5fc43423e73be6ee
Mo Khalil <mkhalil@Central.UH.EDU>
2022-10-21 23:15:11 -0500

    fixed nav bar

--- frondend/src/App.vue
+++ frondend/src/App.vue
@@ -4,7 +4,7 @@ import pages from '~pages'
 </script>
 <template>
     <nav class="text-center">
-    <router-link to="/">Home</router-link> | <router-link to="/about">About</router-link> 
+    <router-link to="/">Home</router-link> |  
     <router-link to="/">Home</router-link> | <router-link to="/about">About</router-link> |
     <router-link to="/contact">Contact</router-link>
   </nav>
