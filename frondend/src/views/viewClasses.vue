179b76585c48c8741dc347f88d04ba3dd47877f5
ichagour <66269867+ichagour@users.noreply.github.com>
2022-10-22 15:23:06 -0500

    Update viewClasses.vue
    Add few lines of code

---
+++ frondend/src/views/viewClasses.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="row justify-content-center">
+   <h1 class="mb-3 mt-3">Classes</h1>
+      <table class="table table-striped">
+      <thead class="table-dark">
+        <tr>
+         <th>Class ID</th>
+         <th>Teacher ID</th>
+         <th>Classdate</th>
+         <th>Classtime</th>
+         <th>Numstudents</th>
+         <th>Maxcapacity</th>
+         <th>Notes</th>
+        </tr>
+      </thead>  
+</template>       
+        
+    
