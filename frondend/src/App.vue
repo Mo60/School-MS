94693f033bb79e66eba2b45a8034d8d4c2c39a10
Mo Khalil <mkhalil@Central.UH.EDU>
2022-10-21 23:11:24 -0500

    added auto router that tracks files in views

--- frondend/src/App.vue
+++ frondend/src/App.vue
@@ -1,5 +1,10 @@
+<script>
+import pages from '~pages'
+
+</script>
 <template>
     <nav class="text-center">
+    <router-link to="/">Home</router-link> | <router-link to="/about">About</router-link> 
     <router-link to="/">Home</router-link> | <router-link to="/about">About</router-link> |
     <router-link to="/contact">Contact</router-link>
   </nav>
