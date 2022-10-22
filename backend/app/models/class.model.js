1fe4bfa37f2f8307ee065b3f6b5fac4ab9d77c05
nickyt123 <78175735+nickyt123@users.noreply.github.com>
2022-10-21 23:21:48 -0500

    Create class.model.js

---
+++ backend/app/models/class.model.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, Sequelize) => {
+  const Class = sequelize.define("class", {
+    ClassID: {
+      type: Sequelize.STRING
+    },
+    TeacherID: {
+      type: Sequelize.STRING
+    },
+    ClassDate: {
+      type: Sequelize.STRING
+    },
+    ClassTime: {
+      type: Sequelize.STRING
+    },
+    NumStudents: {
+      type: Sequelize.STRING
+    },
+    MaxCapacity: {
+      type: Sequelize.STRING
+    },
+    Notes: {
+      type: Sequelize.STRING
+      }
+  });
+
+  return Classes;
+};
