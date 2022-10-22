bea82f3c568913e535e33d1826a130f95eab7848
nickyt123 <78175735+nickyt123@users.noreply.github.com>
2022-10-21 21:54:02 -0500

    Create teacher.model.js

---
+++ backend/app/models/teacher.model.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, Sequelize) => {
+  const Teacher = sequelize.define("teachers", {
+    TeacherID: {
+      type: Sequelize.STRING
+    },
+    FirstName: {
+      type: Sequelize.STRING
+    },
+    LastName: {
+      type: Sequelize.STRING
+    },
+    TeacherPhoneNumber: {
+      type: Sequelize.STRING
+    },
+    TeacherEmail: {
+      type: Sequelize.STRING
+    },
+    Notes: {
+      type: Sequelize.STRING
+      }
+  });
+
+  return Teachers;
+};
