6c1875ae51eaacecd84097e6801e5de0b1ba7055
nickyt123 <78175735+nickyt123@users.noreply.github.com>
2022-10-22 01:32:30 -0500

    update class.routes.js

--- backend/app/routes/class.routes.js
+++ backend/app/routes/class.routes.js
@@ -1,28 +1,25 @@
 module.exports = app => {
-    const class = require("../controllers/class.controller.js");
+    const classes = require("../controllers/class.controller.js");
   
     var router = require("express").Router();
   
-    // Create a new Tutorial
+    // Create a new class
     router.post("/", class.create);
   
     // Retrieve all class
     router.get("/", class.findAll);
   
-    // Retrieve all published class
-    router.get("/published", class.findAllPublished);
-  
     // Retrieve a single Tutorial with id
     router.get("/:id", class.findOne);
   
-    // Update a Tutorial with id
+    // Update a class with id
     router.put("/:id", class.update);
   
-    // Delete a Tutorial with id
+    // Delete a class with id
     router.delete("/:id", class.delete);
   
     // Delete all class
     router.delete("/", class.deleteAll);
   
-    app.use('/api/class', router);
+    app.use('/api/classes', router);
   };
