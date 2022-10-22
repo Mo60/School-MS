c812dc9f46de3d3ef6cb0b2559fdb60fd41aaffc
nickyt123 <78175735+nickyt123@users.noreply.github.com>
2022-10-22 01:42:36 -0500

    update user.routes.js

--- backend/app/routes/user.routes.js
+++ backend/app/routes/user.routes.js
@@ -1,28 +1,25 @@
 module.exports = app => {
-    const user = require("../controllers/user.controller.js");
+    const users = require("../controllers/user.controller.js");
   
     var router = require("express").Router();
   
-    // Create a new Tutorial
+    // Create a new user
     router.post("/", user.create);
   
     // Retrieve all user
     router.get("/", user.findAll);
   
-    // Retrieve all published user
-    router.get("/published", user.findAllPublished);
-  
-    // Retrieve a single Tutorial with id
+    // Retrieve a single user with id
     router.get("/:id", user.findOne);
   
-    // Update a Tutorial with id
+    // Update a user with id
     router.put("/:id", user.update);
   
-    // Delete a Tutorial with id
+    // Delete a user with id
     router.delete("/:id", user.delete);
   
     // Delete all user
     router.delete("/", user.deleteAll);
   
-    app.use('/api/user', router);
+    app.use('/api/users', router);
   };
