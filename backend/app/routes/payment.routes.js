936c8caa747894b87a3472fcf5ebed71041f8855
nickyt123 <78175735+nickyt123@users.noreply.github.com>
2022-10-22 01:41:10 -0500

    update payment.routes.js

--- backend/app/routes/payment.routes.js
+++ backend/app/routes/payment.routes.js
@@ -1,28 +1,25 @@
 module.exports = app => {
-    const payment = require("../controllers/payment.controller.js");
+    const payments = require("../controllers/payment.controller.js");
   
     var router = require("express").Router();
   
-    // Create a new Tutorial
+    // Create a new payment
     router.post("/", payment.create);
   
     // Retrieve all payment
     router.get("/", payment.findAll);
   
-    // Retrieve all published payment
-    router.get("/published", payment.findAllPublished);
-  
-    // Retrieve a single Tutorial with id
+    // Retrieve a single payment with id
     router.get("/:id", payment.findOne);
   
-    // Update a Tutorial with id
+    // Update a payment with id
     router.put("/:id", payment.update);
   
-    // Delete a Tutorial with id
+    // Delete a payment with id
     router.delete("/:id", payment.delete);
   
     // Delete all payment
     router.delete("/", payment.deleteAll);
   
-    app.use('/api/payment', router);
+    app.use('/api/payments', router);
   };
