9a1971cc69bac9cff68360bed77a8366b27b5b5e
nickyt123 <78175735+nickyt123@users.noreply.github.com>
2022-10-22 01:39:00 -0500

    update enrollment.routes.js

--- backend/app/routes/enrollment.routes.js
+++ backend/app/routes/enrollment.routes.js
@@ -1,28 +1,25 @@
 module.exports = app => {
-    const enrollment = require("../controllers/enrollment.controller.js");
+    const enrollments = require("../controllers/enrollment.controller.js");
   
     var router = require("express").Router();
   
-    // Create a new Tutorial
+    // Create a new enrollment
     router.post("/", enrollment.create);
   
     // Retrieve all enrollment
-    router.get("/", enrollment.findAll);
-  
-    // Retrieve all published enrollment
-    router.get("/published", enrollment.findAllPublished);
-  
-    // Retrieve a single Tutorial with id
+    router.get("/", enrollment.findAll);  
+    
+    // Retrieve a single enrollment with id
     router.get("/:id", enrollment.findOne);
   
-    // Update a Tutorial with id
+    // Update a enrollment with id
     router.put("/:id", enrollment.update);
   
-    // Delete a Tutorial with id
+    // Delete a enrollment with id
     router.delete("/:id", enrollment.delete);
   
     // Delete all enrollment
     router.delete("/", enrollment.deleteAll);
   
-    app.use('/api/enrollment', router);
+    app.use('/api/enrollments', router);
   };
