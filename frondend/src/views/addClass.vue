9f8ad45ff1afae5510710b4656db26e7ace0dafa
ichagour <66269867+ichagour@users.noreply.github.com>
2022-10-23 12:57:23 -0500

    Update addClass.vue

---
+++ frondend/src/views/addClass.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+          <h3 class="text-center">Update Classes</h3>
+                      <form @submit.prevent="handleUpdateForm" class="mb-5 pb-5">
+                <div class="form-group">
+                    <label>Class ID</label>
+                    <input type="number" class="form-control" v-model="class.classID" required>
+                </div>
+               <div class="form-group">
+                    <label>Teacher ID</label>
+                    <input type="number" class="form-control" v-model="class.teacherID" required>
+                </div>
+                <div class="form-group">
+                    <label>Class Date</label>
+                    <input type="date" class="form-control" v-model="class.classdate" required>
+                </div>
+                <div class="form-group">
+                    <label>Class Time</label>
+                    <input type="date" class="form-control" v-model="class.classtime" required>
+                    <select class="form-control">
+                    <option value="" disabled selected>Select an Option</option>
+                     <option value="10:00AM - 11:15AM">10:00AM - 11:15AM </option>
+                     <option value="11:15AM - 12:30PM">11:15AM - 12:30PM </option>
+                     <option value="12:30PM - 1:45PM">12:30PM - 1:45PM </option>
+                     <option value="4:00PM - 5:15PM">4:00PM - 5:15PM </option>
+                     <option value="5:15PM - 6:30PM">5:15PM - 6:30PM </option>
+                </select>
+                </div>
+                <div class="form-group">
+                    <label>Number of Students</label>
+                    <input type="number" class="form-control" v-model="class.numstudents" required>
+                </div>
+
+                <div class="form-group">
+                    <label>Max Capacity</label>
+                    <input type="number" class="form-control" v-model="class.maxcapacity" 
+                    required>
+                </div>
+                
+                div class="form-group">
+                    <label>Notes</label>
+                    <input type="text" class="form-control" v-model="class.notes" 
+                    required>
+                </div>
+
+                <div class="text-center m-3">
+                <button class="btn btn-danger mt-3">Add</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from "axios";
+    export default {
+        name: "AddClass",
+        data() {
+            return {
+               class: { 
+                    classID:"",
+                    teacherID: "",
+                    classdate:"",
+                    classtime:"",
+                    numstudents: "",
+                    maxcapacity:"",
+                    notes: ""
+                }, 
+            };
+        },
+        
+    }
+</script>
