d1f33a756d4a227e915f6eada4238e4d0401c651
ichagour <66269867+ichagour@users.noreply.github.com>
2022-10-22 20:25:45 -0500

    Update viewStudents.vue

--- frondend/src/views/viewStudents.vue
+++ frondend/src/views/viewStudents.vue
@@ -1,5 +1,25 @@
 <template>
-    <h1>Hello</h1>
+   <h1>Hello</h1>
+   <div class="row justify-content-center">
+   <h1 class="mb-3 mt-3">Invoices</h1>
+      <table class="table table-striped">
+      <thead class="table-dark">
+        <tr>
+         <th>Student ID</th>
+         <th>Parent ID</th>
+         <th>First Name</th>
+         <th>Last Name</th>
+         <th>DOB</th>
+         <th>StartDate</th>
+         <th>Lesson Day</th>
+         <th>Lesson Time</th>
+         <th>Emergency Contact Name</th>
+         <th>Emergency Contact Phone</th>
+         <th>Medical</th>
+         <th>Notes</th>
+        </tr>
+      </thead>  
+  </table></div> 
 </template>
 <script>
 export default{
