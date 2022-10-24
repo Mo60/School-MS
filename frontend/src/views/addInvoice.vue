49cb8cd869b428bf9d5f8a74155b1675f73ef6a2
ichagour <66269867+ichagour@users.noreply.github.com>
2022-10-23 19:04:59 -0500

    Update addInvoice.vue

---
+++ frontend/src/views/addInvoice.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+          <h3 class="text-center">Update Invoices</h3>
+                      <form @submit.prevent="handleUpdateForm" class="mb-5 pb-5">
+                <div class="form-group">
+                    <label>Invoice ID</label>
+                    <input type="number" class="form-control"  required>
+                </div>
+                
+                <div class="form-group">
+                    <label>Student ID</label>
+                    <input type="number" class="form-control"  required>
+                </div>
+                
+               <div class="form-group">
+                    <label>Total</label>
+                    <input type="number" class="form-control"  required>
+                </div>
+                
+                <div class="form-group">
+                    <label>Totalpayment</label>
+                    <input type="number" class="form-control"  required>
+                </div>
+                
+                <div class="form-group">
+                    <label>Due Date</label>
+                    <input type="date" class="form-control"  required>
+                </div>
+                
+                <div class="text-center m-3">
+                <button class="btn btn-danger mt-3">Add</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from "axios";
+    export default {
+        name: "AddInvoice",
+        data() {
+            return {
+               Invoice: { 
+                    InvoiceID:"",
+                    StudentID: "",
+                    Total:"",
+                    Totalpayment:"",
+                    Duedate: ""
+                }, 
+            };
+        },
+        
+    }
+</script>
