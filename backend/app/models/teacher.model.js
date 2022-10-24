e9149541ab69ab9623feb11a6b1b647f5f90ba37
nickyt123 <78175735+nickyt123@users.noreply.github.com>
2022-10-24 01:30:16 -0500

    Update teacher.model.js

--- backend/app/models/teacher.model.js
+++ backend/app/models/teacher.model.js
@@ -1,7 +1,7 @@
 module.exports = (sequelize, Sequelize) => {
   const Teacher = sequelize.define("teacher", {
     TeacherID: {
-      type: Sequelize.STRING
+      type: Sequelize.INTEGER
     },
     FirstName: {
       type: Sequelize.STRING
