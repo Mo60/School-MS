199c6ef295aef96a1f4251e395031c19138f4994
alexiscenriquez <alexiscenriquez@hotmail.com>
2022-10-25 16:17:11 -0500

    updates

--- frontend/src/auth/UserAuth.vue
+++ frontend/src/auth/UserAuth.vue
@@ -1,47 +1,94 @@
 <template>
-    <form action="">
-    <div class="loginwrapper">
-        <h1>Login</h1>
-        <form  class="mt-4" @submit.prevent="submitForm">
-            <fieldset class="form-control">
-               <div class="col">
-                    <label for="" class="form-label">User Name</label>
-                    <input type="text" class="form-control mb-3" v-model.trim="userName">
-                    <label for="" class="form-label" >Password</label>
-                    <input type="password" class="form-control mb-3" v-model.trim="password">
-               </div >
-               <button class="btn">Login</button>
-               <button class="btn">Sign Up</button>
-            </fieldset >
-<p v-if="!formIsValid">Please enter a valid username and password</p>
-        </form>
+ 
+    <div class="wrapper2">
+      <h1 class="mt-5 mb-5">Login</h1>
+      <form class="mt-4" @submit.prevent="login">
+        <fieldset class="form-control p-5">
+          <div class="col">
+            <label for="" class="form-label">Email</label>
+            <input
+              type="email"
+              class="form-control mb-5"
+              v-model.trim="email"
+            />
+            <label for="" class="form-label">Password</label>
+            <input
+              type="password"
+              class="form-control mb-5"
+              v-model.trim="password"
+            />
+          </div>
+          <div class="btns">
+            <button class="btn">{{submitButtonCaption}}</button>
+            <button class="btn" mode="flat" @click="switchAuthMode">{{swithModeButtonCaption}}</button>
+          </div>
+        </fieldset>
+        <p v-if="!formIsValid">Please enter a valid email and password</p>
+      </form>
     </div>
-    </form>
+ 
 </template>
 
     <!-- <RouterView></RouterView>  -->
 
  
 <script>
-import router from '../router';
-
-export default{
-    data(){
-        return{
-userName:"",
-password:"",
-formIsValid:true
-        }
-    },
-    methods:{
-      submitForm(){
-        this.formIsValid=true
-        if(this.userName===""||this.password.length<6)
-        {
-            this.formIsValid=false
-        }
-        else this.$router.push("/")
-      }  
-    }
+import router from "../router";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      formIsValid: true,
+      mode: "Login",
+    };
+  },
+  computed:{
+    submitButtonCaption(){
+if(this.mode==='Login')
+    return 'Login'
+    else return 'Sign Up'
+},
+    swithModeButtonCaption(){if(this.mode==='Login')
+    return 'Sign Up Instead'
+    else return 'Login Instead'
 }
+  },
+  methods: {
+    login() {
+      this.formIsValid = true;
+      if (this.userName === "" || this.password.length < 6) {
+        this.formIsValid = false;
+      } 
+
+     
+
+const auth = getAuth();
+signInWithEmailAndPassword(auth, this.email, this.password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+
+  this.$router.push("/")
+    },
+
+    switchAuthMode() {
+      if (this.mode === "Login") {
+        this.mode = "Sign Up";
+      }
+
+      else {
+        this.mode="Login"
+      }
+    },
+  },
+};
 </script>
+
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,6 +7,26 @@ import router from './router'
 // Import all of Bootstrap's JS
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap"
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 const app= createApp(App)
 app.use(router)
 app.mount("#app")
+
+  // Import the functions you need from the SDKs you need
+ 
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+  
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAvh1qv6ivO9-wvbKM4QQm8IwDzvhGgtIM",
+    authDomain: "cis4375project-15020.firebaseapp.com",
+    projectId: "cis4375project-15020",
+    storageBucket: "cis4375project-15020.appspot.com",
+    messagingSenderId: "480313408794",
+    appId: "1:480313408794:web:25867f87ef5d30f32a00a3",
+    measurementId: "G-8TJ0L0GTEL"
+  };
+  
