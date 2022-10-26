de4fc0720c1005b6c54bb53fb1d3c7e73ecbbbbb
nickyt123 <78175735+nickyt123@users.noreply.github.com>
2022-10-25 23:49:38 -0500

    Create enrollment.controller.js

---
+++ backend/app/controllers/enrollment.controller.js
@@ -0,0 +1,175 @@
+const db = require("../models");
+const enrollment = db.enrollments;
+const Op = db.Sequelize.Op;
+
+// Create and Save a new enrollment
+exports.create = (req, res) => {
+  
+};
+
+// Retrieve all enrollments from the database.
+exports.findAll = (req, res) => {
+  
+};
+
+// Find a single enrollment with an id
+exports.findOne = (req, res) => {
+  
+};
+
+// Update a enrollment by the id in the request
+exports.update = (req, res) => {
+  
+};
+
+// Delete a enrollment with the specified id in the request
+exports.delete = (req, res) => {
+  
+};
+
+// Delete all enrollments from the database.
+exports.deleteAll = (req, res) => {
+  
+};
+
+// Find all published enrollments
+exports.findAllPublished = (req, res) => {
+  
+};
+
+exports.create = (req, res) => {
+    // Validate request
+    if (!req.body.title) {
+      res.status(400).send({
+        message: "Content can not be empty!"
+      });
+      return;
+    }
+  
+    // Create a enrollment
+    const enrollment = {
+      title: req.body.title,
+      description: req.body.description,
+      published: req.body.published ? req.body.published : false
+    };
+  
+    // Save enrollment in the database
+    enrollment.create(enrollment)
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while creating the enrollment."
+        });
+      });
+  };
+
+  exports.findAll = (req, res) => {
+    const title = req.query.title;
+    var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
+  
+    enrollment.findAll({ where: condition })
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving enrollments."
+        });
+      });
+  };
+
+  exports.findOne = (req, res) => {
+    const id = req.params.id;
+  
+    enrollment.findByPk(id)
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Error retrieving enrollment with id=" + id
+        });
+      });
+  };
+
+  exports.update = (req, res) => {
+    const id = req.params.id;
+  
+    enrollment.update(req.body, {
+      where: { id: id }
+    })
+      .then(num => {
+        if (num == 1) {
+          res.send({
+            message: "enrollment was updated successfully."
+          });
+        } else {
+          res.send({
+            message: `Cannot update enrollment with id=${id}. Maybe enrollment was not found or req.body is empty!`
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Error updating enrollment with id=" + id
+        });
+      });
+  };
+
+  exports.delete = (req, res) => {
+    const id = req.params.id;
+  
+    enrollment.destroy({
+      where: { id: id }
+    })
+      .then(num => {
+        if (num == 1) {
+          res.send({
+            message: "enrollment was deleted successfully!"
+          });
+        } else {
+          res.send({
+            message: `Cannot delete enrollment with id=${id}. Maybe enrollment was not found!`
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Could not delete enrollment with id=" + id
+        });
+      });
+  };
+
+  
+  exports.deleteAll = (req, res) => {
+    enrollment.destroy({
+      where: {},
+      truncate: false
+    })
+      .then(nums => {
+        res.send({ message: `${nums} enrollments were deleted successfully!` });
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while removing all enrollments."
+        });
+      });
+  };
+
+  exports.findAllPublished = (req, res) => {
+    enrollment.findAll({ where: { published: true } })
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving enrollments."
+        });
+      });
+  };
