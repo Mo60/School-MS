181a57dbdf7dc42cfb48aefc41c52c9ebe00aec1
alexiscenriquez <alexiscenriquez@hotmail.com>
2022-10-28 04:21:24 -0500

    fixed format on invoices form

--- frontend/src/views/addInvoice.vue
+++ frontend/src/views/addInvoice.vue
@@ -1,28 +1,30 @@
 <template>
     <h1 class="mt-5">Add Invoices</h1>
-    <div class="wrapper2">   
+    <div class="wrapper">   
                       <form @submit.prevent="handleUpdateForm" class="mb-5 pb-5">
             <fieldset class="form-control p-4">
-                
-                    <div class="col mb-3">
-                        <label class="form-label">Student ID</label>
-                        <input type="number" class="form-control"  required>
-                    </div>
+                <div class="row">
                     
-                   <div class="col mb-3">
-                        <label class="form-label">Total</label>
-                        <input type="number" class="form-control" min="0"  required>
-                    </div>
-                    
-                    <div class="col mb-3">
-                        <label class="form-label">Total Payment</label>
-                        <input type="number" class="form-control" min="0" required>
-                    </div>
-                    
-                    <div class="col mb-3">
-                        <label class="form-label">Due Date</label>
-                        <input type="date" class="form-control"  required>
-                    </div>
+                        <div class="col mb-3">
+                            <label class="form-label">Student ID</label>
+                            <input type="number" class="form-control"  required>
+                        </div>
+                        
+                       <div class="col mb-3">
+                            <label class="form-label">Total</label>
+                            <input type="number" class="form-control" min="0"  required>
+                        </div>
+                        
+                        <div class="col mb-3">
+                            <label class="form-label">Total Payment</label>
+                            <input type="number" class="form-control" min="0" required>
+                        </div>
+                        
+                        <div class="col mb-3">
+                            <label class="form-label">Due Date</label>
+                            <input type="date" class="form-control"  required>
+                        </div>
+                </div >
                     
                     <div class="text-center m-3">
                     
