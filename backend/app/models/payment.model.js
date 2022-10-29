c0e0ac4d2c28eaa3ed1e7fcb05b512b840a954fa
nickyt123 <78175735+nickyt123@users.noreply.github.com>
2022-10-28 20:54:57 -0500

    Update payment.model.js

--- backend/app/models/payment.model.js
+++ backend/app/models/payment.model.js
@@ -17,8 +17,8 @@ module.exports = (sequelize, Sequelize) => {
     InvoiceID: {
       type: Sequelize.INTEGER,
       references: {
-          model: 'invoice'
-          key: 'InvoiceID'
+          model: 'invoice',
+          key: 'InvoiceID',
         }
     }
   }, 
