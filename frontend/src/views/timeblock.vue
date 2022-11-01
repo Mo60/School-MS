88b24899ca20fef63b96b02540d0803f663e76c4
ichagour <66269867+ichagour@users.noreply.github.com>
2022-11-01 11:51:55 -0500

    Create timeblock.vue
    TimeblockView

---
+++ frontend/src/views/timeblock.vue
@@ -0,0 +1,39 @@
+<div class="row justify-content-center">
+   <h1 class="mb-3 mt-3">Timeblock</h1>
+      <table class="table table-striped">
+      <thead class="table-dark">
+        <tr>
+          <th>Timeblock ID</th>
+          <th>Start Time</th>
+          <th>End Time</th>
+        </tr>
+      </thead>
+      <tbody>   
+        <tr v-for="t in Timeblock" :key="t._id">
+            <td>{{t.TimeblockID }}</td>
+            <td>{{t.StartTime }}</td>
+             <td>{{t.EndTime }}</td>
+          </tr>
+      </tbody>
+    </table>    
+ </div>
+</template>
+<script>
+    import axios from "axios";
+
+    export default {
+        data() {
+            return {
+                Timeblock: []
+            }
+        },
+        created() {
+            let apiURL = '';
+            axios.get(apiURL).then(res => {
+                this.Timeblock = res.data;
+            }).catch(error => {
+                console.log(error)
+            });
+        }
+    }
+</script>
