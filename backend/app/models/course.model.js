32f2f165e44037180127a259093404148d88f093
nickyt123 <78175735+nickyt123@users.noreply.github.com>
2022-11-01 02:49:30 -0500

    Create course.model.js

---
+++ backend/app/models/course.model.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, Sequelize) => {
+  const Course = sequelize.define("course", {
+    
+    
+    CourseID: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    CourseName: {
+      type: Sequelize.STRING
+    },
+    Description: {
+      type: Sequelize.STRING
+    },
+    CourseStatusID: {
+      type: Sequelize.INTEGER,
+            references: {
+          model: 'courseStatus', 
+          key: 'CourseStatusID', 
+        }
+        }
+  
+  }, 
+  { tableName: "course",
+  timestamps: false,
+  createdAt: false,
+  updatedAt: false,});
+
+  return Course;
+};
