f85b42a8ed0d39548b7c553ecafe5190b54a0d95
nickyt123 <78175735+nickyt123@users.noreply.github.com>
2022-11-01 03:00:30 -0500

    Create facultyStatus,model.js

---
+++ backend/app/models/facultyStatus,model.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, Sequelize) => {
+  const FacultyStatus = sequelize.define("facultyStatus", {
+    FacultyStatusID: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    Status: {
+      type: Sequelize.STRING
+    },
+  },
+                           
+                               
+  {tableName: "facultyStatus",
+  timestamps: false,
+  createdAt: false,
+  updatedAt: false,});
+
+  return FacultyStatus;
+};
