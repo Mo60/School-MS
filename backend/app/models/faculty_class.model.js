c052b1b9a7e00f5c5fd59674aa30dfc62263af5f
nickyt123 <78175735+nickyt123@users.noreply.github.com>
2022-11-01 02:57:10 -0500

    Create faculty_class.model.js

---
+++ backend/app/models/faculty_class.model.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, Sequelize) => {
+  const Faculty_Class = sequelize.define("faculty_class", {
+    
+    _id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    ClassID: {
+      type: Sequelize.INTEGER,
+      references: {
+          model: 'ClassID', 
+          key: 'class', 
+        }
+    },
+    FacultyID: {
+      type: Sequelize.INTEGER,
+      references: {
+          model: 'FacultyID', 
+          key: 'faculty', 
+        }
+        }
+    },
+  }, 
+  { tableName: "faculty_class",
+  timestamps: false,
+  createdAt: false,
+  updatedAt: false,});
+
+  return Facukty_Class;
+};
