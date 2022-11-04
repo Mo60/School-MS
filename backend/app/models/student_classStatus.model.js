ef6da6c4ce03c99f123af4b316c67d8079f26218
nickyt123 <78175735+nickyt123@users.noreply.github.com>
2022-11-03 21:18:24 -0500

    Create student_classStatus.model.js

---
+++ backend/app/models/student_classStatus.model.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, Sequelize) => {
+  const Student_ClassStatus = sequelize.define("student_classStatus", {
+    StudentClassStatusID: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    Status: {
+      type: Sequelize.STRING
+    },
+  },
+                           
+                               
+  {tableName: "student_classStatus",
+  timestamps: false,
+  createdAt: false,
+  updatedAt: false,});
+
+  return Student_ClassStatus;
+};
