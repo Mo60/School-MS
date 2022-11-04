fa8ea69c6b453babb1029e4579d736856669808d
nickyt123 <78175735+nickyt123@users.noreply.github.com>
2022-11-03 21:22:20 -0500

    Create room.model.js

---
+++ backend/app/models/room.model.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, Sequelize) => {
+  const Room = sequelize.define("room", {
+    RoomID: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    LocationID: {
+      type: Sequelize.INTEGER,
+      references: {
+          model: 'location', 
+          key: 'LocationID', 
+        }
+    },
+    Capacity: {
+      type: Sequelize.STRING
+    },
+    RoomName: {
+      type: Sequelize.STRING
+    },
+    RoomStatusID: {
+      type: Sequelize.INTEGER,
+      references: {
+          model: 'roomStatus', 
+          key: 'RoomStatusID', 
+        }
+    },
+  }, 
+                           
+                               
+  {tableName: "room",
+  timestamps: false,
+  createdAt: false,
+  updatedAt: false,});
+
+  return Room;
+};
