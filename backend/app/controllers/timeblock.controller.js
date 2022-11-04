bb4144d0687b8d4a130f3e13e6a5624687e46591
nickyt123 <78175735+nickyt123@users.noreply.github.com>
2022-11-04 03:33:11 -0500

    Update timeblock.controller.js

--- backend/app/controllers/timeblock.controller.js
+++ backend/app/controllers/timeblock.controller.js
@@ -60,10 +60,10 @@ exports.create = (req, res) => {
 // bulk
  exports.createmany = (req, res) => {
     // Create a guardian
-    const guardians = req.body;
+    const timeblocks = req.body;
   
     // Save guardian in the database
-    Timeblock.bulkCreate(guardians)
+    Timeblock.bulkCreate(timeblocks)
       .then(data => {
         res.send(data);
       })
