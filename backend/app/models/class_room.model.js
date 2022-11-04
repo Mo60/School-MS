ff6904a24544ee8486f9e0677eeb93242a73ca35
nickyt123 <78175735+nickyt123@users.noreply.github.com>
2022-11-03 21:20:04 -0500

    Create class_room.model.js

---
+++ backend/app/models/class_room.model.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, Sequelize) => {
+  const Class_Room = sequelize.define("class_room", {
+    _id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    ClassID: {
+      type: Sequelize.INTEGER,
+      references: {
+          model: 'class', 
+          key: 'ClassID', 
+        }
+    },
+    RoomID: {
+      type: Sequelize.INTEGER,
+      references: {
+          model: 'room', 
+          key: 'RoomID', 
+        }
+    },
+  },
+                           
+                               
+  {tableName: "class_room",
+  timestamps: false,
+  createdAt: false,
+  updatedAt: false,});
+
+  return Class_Room;
+};
