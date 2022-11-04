134327d53da03abe0ddecb1c64072f53082ca221
nickyt123 <78175735+nickyt123@users.noreply.github.com>
2022-11-04 01:22:52 -0500

    Create locationStatus.routes.js

---
+++ backend/app/routes/locationStatus.routes.js
@@ -0,0 +1,25 @@
+module.exports = app => {
+    const locationStatus = require("../controllers/locationStatus.controller.js");
+  
+    var router = require("express").Router();
+  
+    // Create a new locationStatus  router.post("/", locationStatus.create);
+    router.post("/bulk", locationStatus.createmany);
+  
+    // Retrieve all locationStatus(s)
+    router.get("/", locationStatus.findAll);
+  
+    // Retrieve a single locationStatus with id
+    router.get("/:id", locationStatus.findOne);
+  
+    // Update a locationStatus with id
+    router.put("/:id", locationStatus.update);
+  
+    // Delete a locationStatus with id
+    router.delete("/:id", locationStatus.delete);
+  
+    // Delete all locationStatus(s)
+    router.delete("/", locationStatus.deleteAll);
+  
+    app.use('/api/locationStatus', router);
+  };
