0fe9b9d54dda6f46ef5a2a56ef11d0aca93b28f3
nickyt123 <78175735+nickyt123@users.noreply.github.com>
2022-11-03 21:36:47 -0500

    Update student.model.js

--- backend/app/models/student.model.js
+++ backend/app/models/student.model.js
@@ -31,7 +31,7 @@ module.exports = (sequelize, Sequelize) => {
       type: Sequelize.STRING
     },
     Zip: {
-      type: Sequelize.STRING
+      type: Sequelize.INTEGER
     },
     StudentStatusID: {
       type: Sequelize.INTEGER,
