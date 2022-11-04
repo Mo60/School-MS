c134abbcb6f4082f365dbbc33ed33a26cac0915f
nickyt123 <78175735+nickyt123@users.noreply.github.com>
2022-11-04 03:32:26 -0500

    Update guardianStatus.controller.js

--- backend/app/controllers/guardianStatus.controller.js
+++ backend/app/controllers/guardianStatus.controller.js
@@ -59,10 +59,10 @@ exports.create = (req, res) => {
 // bulk
  exports.createmany = (req, res) => {
     // Create a guardian
-    const guardians = req.body;
+    const guardianStatuses = req.body;
   
     // Save guardian in the database
-    GuardianStatus.bulkCreate(guardians)
+    GuardianStatus.bulkCreate(guardianStatuses)
       .then(data => {
         res.send(data);
       })
