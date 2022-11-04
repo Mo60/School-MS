4400a1c6758aeef276b2a30383be8e6abdb08d71
nickyt123 <78175735+nickyt123@users.noreply.github.com>
2022-11-03 21:14:09 -0500

    Create medical.model.js

---
+++ backend/app/models/medical.model.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, Sequelize) => {
+  const Medical = sequelize.define("medical", {
+    
+    MedicalID: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    Condition: {
+      type: Sequelize.STRING
+    },
+  
+  }, 
+  { tableName: "medical",
+  timestamps: false,
+  createdAt: false,
+  updatedAt: false,});
+
+  return Medical;
+};
