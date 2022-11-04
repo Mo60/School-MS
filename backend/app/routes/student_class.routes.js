2cefa20d3ad021ee19a21d90bbf8541a376c0450
nickyt123 <78175735+nickyt123@users.noreply.github.com>
2022-11-03 23:18:56 -0500

    create student_class.routes.js

---
+++ backend/app/routes/student_class.routes.js
@@ -0,0 +1,26 @@
+module.exports = app => {
+    const student_class = require("../controllers/student_class.controller.js");
+  
+    var router = require("express").Router();
+  
+    // Create a new student_class
+    router.post("/", student_class.create);
+    router.post("/bulk", student_class.createmany);
+  
+    // Retrieve all student_class(s)
+    router.get("/", student_class.findAll);
+  
+    // Retrieve a single student_class with id
+    router.get("/:id", student_class.findOne);
+  
+    // Update a student_class with id
+    router.put("/:id", student_class.update);
+  
+    // Delete a student_class with id
+    router.delete("/:id", student_class.delete);
+  
+    // Delete all student_class(s)
+    router.delete("/", student_class.deleteAll);
+  
+    app.use('/api/student_class', router);
+  };
