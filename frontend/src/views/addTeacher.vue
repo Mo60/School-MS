bdecd9b4ec7acec7ea52c015d25e5292dcc41a8c
ichagour <66269867+ichagour@users.noreply.github.com>
2022-11-04 23:02:04 -0500

    Update addTeacher.vue

--- frontend/src/views/addTeacher.vue
+++ frontend/src/views/addTeacher.vue
@@ -72,11 +72,11 @@
 
         methods:{
             submitForm(){
-                let apiURL = "http://172.26.54.21:8082/api/faculty/"
+                let apiURL = "http://172.26.54.21:8082/api/teacher/"
                 axios.post(apiURL,this.faculty).catch(error => {
         console.log(error)
       });
-      this.$router.push("/faculty")
+      this.$router.push("/teachers")
 } 
      }
         }
