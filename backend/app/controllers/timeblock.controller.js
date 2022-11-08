e685834c23d3bf81e099af517a94cd6613bc0b37
nickyt123 <78175735+nickyt123@users.noreply.github.com>
2022-11-07 19:56:50 -0600

    Update timeblock.controller.js

--- backend/app/controllers/timeblock.controller.js
+++ backend/app/controllers/timeblock.controller.js
@@ -2,32 +2,32 @@ const db = require("../models");
 const Timeblock = db.timeblock;
 const Op = db.Sequelize.Op;
 
-// Create and Save a new guardian
+// Create and Save a new timeblock
 exports.create = (req, res) => {
   
 };
 
-// Retrieve all guardians from the database.
+// Retrieve all timeblocks from the database.
 exports.findAll = (req, res) => {
   
 };
 
-// Find a single guardian with an id
+// Find a single timeblock with an id
 exports.findOne = (req, res) => {
   
 };
 
-// Update a guardian by the id in the request
+// Update a timeblock by the id in the request
 exports.update = (req, res) => {
   
 };
 
-// Delete a guardian with the specified id in the request
+// Delete a timeblock with the specified id in the request
 exports.delete = (req, res) => {
   
 };
 
-// Delete all guardians from the database.
+// Delete all timeblocks from the database.
 exports.deleteAll = (req, res) => {
   
 };
@@ -36,14 +36,14 @@ exports.deleteAll = (req, res) => {
 
 exports.create = (req, res) => {
   
-    // Create a guardian
+    // Create a timeblock
     const timeblock = {
       TimeblockID: req.body.TimeblockID,
       StartTime: req.body.StartTime,
       EndTime: req.body.EndTime
     };
   
-    // Save guardian in the database
+    // Save timeblock in the database
     Timeblock.create(timeblock)
     .then(data => {
       res.send(data);
@@ -51,7 +51,7 @@ exports.create = (req, res) => {
     .catch(err => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while creating the guardianRelationship."
+          err.message || "Some error occurred while creating the timeblockRelationship."
       });
     });
     }
@@ -59,10 +59,10 @@ exports.create = (req, res) => {
   ;
 // bulk
  exports.createmany = (req, res) => {
-    // Create a guardian
+    // Create a timeblock
     const timeblocks = req.body;
   
-    // Save guardian in the database
+    // Save timeblock in the database
     Timeblock.bulkCreate(timeblocks)
       .then(data => {
         res.send(data);
@@ -70,7 +70,7 @@ exports.create = (req, res) => {
       .catch(err => {
         res.status(500).send({
           message:
-            err.message || "Some error occurred while creating the guardian."
+            err.message || "Some error occurred while creating the timeblock."
         });
       });
   };
@@ -83,7 +83,7 @@ exports.create = (req, res) => {
       .catch(err => {
         res.status(500).send({
           message:
-            err.message || "Some error occurred while retrieving guardians."
+            err.message || "Some error occurred while retrieving timeblocks."
         });
       });
   };
@@ -97,7 +97,7 @@ exports.create = (req, res) => {
       })
       .catch(err => {
         res.status(500).send({
-          message: "Error retrieving Guardian_student with id=" + id
+          message: "Error retrieving timeblock_student with id=" + id
         });
       });
   };
@@ -106,22 +106,22 @@ exports.create = (req, res) => {
     const id = req.params.id;
   
     Timeblock.update(req.body, {
-      where: { _id: id }
+      where: { TimeblockID: id }
     })
       .then(num => {
         if (num == 1) {
           res.send({
-            message: "guardian was updated successfully."
+            message: "timeblock was updated successfully."
           });
         } else {
           res.send({
-            message: `Cannot update guardian with id=${id}. Maybe guardian was not found or req.body is empty!`
+            message: `Cannot update timeblock with id=${id}. Maybe timeblock was not found or req.body is empty!`
           });
         }
       })
       .catch(err => {
         res.status(500).send({
-          message: "Error updating guardian with id=" + id
+          message: "Error updating timeblock with id=" + id
         });
       });
   };
@@ -130,22 +130,22 @@ exports.create = (req, res) => {
     const id = req.params.id;
   
     Timeblock.destroy({
-      where: { _id: id }
+      where: { TimeblockID: id }
     })
       .then(num => {
         if (num == 1) {
           res.send({
-            message: "guardian was deleted successfully!"
+            message: "timeblock was deleted successfully!"
           });
         } else {
           res.send({
-            message: `Cannot delete guardian with id=${id}. Maybe guardian was not found!`
+            message: `Cannot delete timeblock with id=${id}. Maybe timeblock was not found!`
           });
         }
       })
       .catch(err => {
         res.status(500).send({
-          message: "Could not delete guardian with id=" + id
+          message: "Could not delete timeblock with id=" + id
         });
       });
   };
@@ -157,12 +157,12 @@ exports.create = (req, res) => {
       truncate: false
     })
       .then(nums => {
-        res.send({ message: `${nums} guardians were deleted successfully!` });
+        res.send({ message: `${nums} timeblocks were deleted successfully!` });
       })
       .catch(err => {
         res.status(500).send({
           message:
-            err.message || "Some error occurred while removing all guardians."
+            err.message || "Some error occurred while removing all timeblocks."
         });
       });
   };
@@ -175,7 +175,7 @@ exports.create = (req, res) => {
       .catch(err => {
         res.status(500).send({
           message:
-            err.message || "Some error occurred while retrieving guardians."
+            err.message || "Some error occurred while retrieving timeblocks."
         });
       });
   };
