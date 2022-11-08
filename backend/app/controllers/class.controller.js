c752000c469b30059f8ddf25ebe65bddf7f40984
nickyt123 <78175735+nickyt123@users.noreply.github.com>
2022-11-07 19:17:52 -0600

    Update class.controller.js

--- backend/app/controllers/class.controller.js
+++ backend/app/controllers/class.controller.js
@@ -173,7 +173,7 @@ exports.create = (req, res) => {
       .catch(err => {
         res.status(500).send({
           message:
-            err.message || "Some error occurred while retrieving Classs."
+            err.message || "Some error occurred while retrieving Class."
         });
       });
   };
