3d7e45620d1b931b8e77e1c4528fbe2df066d6b5
nickyt123 <78175735+nickyt123@users.noreply.github.com>
2022-11-07 19:40:31 -0600

    Update faculty.controller.js

--- backend/app/controllers/faculty.controller.js
+++ backend/app/controllers/faculty.controller.js
@@ -2,32 +2,32 @@ const db = require("../models");
 const Faculty = db.faculty;
 const Op = db.Sequelize.Op;
 
-// Create and Save a new guardian
+// Create and Save a new faculty
 exports.create = (req, res) => {
   
 };
 
-// Retrieve all guardians from the database.
+// Retrieve all facultys from the database.
 exports.findAll = (req, res) => {
   
 };
 
-// Find a single guardian with an id
+// Find a single faculty with an id
 exports.findOne = (req, res) => {
   
 };
 
-// Update a guardian by the id in the request
+// Update a faculty by the id in the request
 exports.update = (req, res) => {
   
 };
 
-// Delete a guardian with the specified id in the request
+// Delete a faculty with the specified id in the request
 exports.delete = (req, res) => {
   
 };
 
-// Delete all guardians from the database.
+// Delete all facultys from the database.
 exports.deleteAll = (req, res) => {
   
 };
@@ -36,7 +36,7 @@ exports.deleteAll = (req, res) => {
 
 exports.create = (req, res) => {
   
-    // Create a guardian
+    // Create a faculty
     const faculty = {
       FacultyID: req.body.FacultyID,
       FirstName: req.body.FirstName,
@@ -48,7 +48,7 @@ exports.create = (req, res) => {
       FacultyStatusID: req.body.FacultyStatusID
     };
   
-    // Save guardian in the database
+    // Save faculty in the database
     Faculty.create(faculty)
     .then(data => {
       res.send(data);
@@ -56,7 +56,7 @@ exports.create = (req, res) => {
     .catch(err => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while creating the guardianRelationship."
+          err.message || "Some error occurred while creating the facultyRelationship."
       });
     });
     }
@@ -64,10 +64,10 @@ exports.create = (req, res) => {
   ;
 // bulk
  exports.createmany = (req, res) => {
-    // Create a guardian
+    // Create a faculty
     const faculties = req.body;
   
-    // Save guardian in the database
+    // Save faculty in the database
     Faculty.bulkCreate(faculties)
       .then(data => {
         res.send(data);
@@ -75,7 +75,7 @@ exports.create = (req, res) => {
       .catch(err => {
         res.status(500).send({
           message:
-            err.message || "Some error occurred while creating the guardian."
+            err.message || "Some error occurred while creating the faculty."
         });
       });
   };
@@ -88,7 +88,7 @@ exports.create = (req, res) => {
       .catch(err => {
         res.status(500).send({
           message:
-            err.message || "Some error occurred while retrieving guardians."
+            err.message || "Some error occurred while retrieving facultys."
         });
       });
   };
@@ -102,7 +102,7 @@ exports.create = (req, res) => {
       })
       .catch(err => {
         res.status(500).send({
-          message: "Error retrieving Guardian_student with id=" + id
+          message: "Error retrieving faculty_student with id=" + id
         });
       });
   };
@@ -111,22 +111,22 @@ exports.create = (req, res) => {
     const id = req.params.id;
   
     Faculty.update(req.body, {
-      where: { _id: id }
+      where: { FacultyID: id }
     })
       .then(num => {
         if (num == 1) {
           res.send({
-            message: "guardian was updated successfully."
+            message: "faculty was updated successfully."
           });
         } else {
           res.send({
-            message: `Cannot update guardian with id=${id}. Maybe guardian was not found or req.body is empty!`
+            message: `Cannot update faculty with id=${id}. Maybe faculty was not found or req.body is empty!`
           });
         }
       })
       .catch(err => {
         res.status(500).send({
-          message: "Error updating guardian with id=" + id
+          message: "Error updating faculty with id=" + id
         });
       });
   };
@@ -135,22 +135,22 @@ exports.create = (req, res) => {
     const id = req.params.id;
   
     Faculty.destroy({
-      where: { _id: id }
+      where: { FacultyID: id }
     })
       .then(num => {
         if (num == 1) {
           res.send({
-            message: "guardian was deleted successfully!"
+            message: "faculty was deleted successfully!"
           });
         } else {
           res.send({
-            message: `Cannot delete guardian with id=${id}. Maybe guardian was not found!`
+            message: `Cannot delete faculty with id=${id}. Maybe faculty was not found!`
           });
         }
       })
       .catch(err => {
         res.status(500).send({
-          message: "Could not delete guardian with id=" + id
+          message: "Could not delete faculty with id=" + id
         });
       });
   };
@@ -162,12 +162,12 @@ exports.create = (req, res) => {
       truncate: false
     })
       .then(nums => {
-        res.send({ message: `${nums} guardians were deleted successfully!` });
+        res.send({ message: `${nums} facultys were deleted successfully!` });
       })
       .catch(err => {
         res.status(500).send({
           message:
-            err.message || "Some error occurred while removing all guardians."
+            err.message || "Some error occurred while removing all facultys."
         });
       });
   };
@@ -180,7 +180,7 @@ exports.create = (req, res) => {
       .catch(err => {
         res.status(500).send({
           message:
-            err.message || "Some error occurred while retrieving guardians."
+            err.message || "Some error occurred while retrieving facultys."
         });
       });
   };
