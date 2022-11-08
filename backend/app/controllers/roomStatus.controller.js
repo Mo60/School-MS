c49361e386c9a35582556973ed00da27d4102499
nickyt123 <78175735+nickyt123@users.noreply.github.com>
2022-11-07 19:51:04 -0600

    Update roomStatus.controller.js

--- backend/app/controllers/roomStatus.controller.js
+++ backend/app/controllers/roomStatus.controller.js
@@ -2,32 +2,32 @@ const db = require("../models");
 const RoomStatus = db.roomStatus;
 const Op = db.Sequelize.Op;
 
-// Create and Save a new guardian
+// Create and Save a new roomStatus
 exports.create = (req, res) => {
   
 };
 
-// Retrieve all guardians from the database.
+// Retrieve all roomStatuss from the database.
 exports.findAll = (req, res) => {
   
 };
 
-// Find a single guardian with an id
+// Find a single roomStatus with an id
 exports.findOne = (req, res) => {
   
 };
 
-// Update a guardian by the id in the request
+// Update a roomStatus by the id in the request
 exports.update = (req, res) => {
   
 };
 
-// Delete a guardian with the specified id in the request
+// Delete a roomStatus with the specified id in the request
 exports.delete = (req, res) => {
   
 };
 
-// Delete all guardians from the database.
+// Delete all roomStatuss from the database.
 exports.deleteAll = (req, res) => {
   
 };
@@ -36,13 +36,13 @@ exports.deleteAll = (req, res) => {
 
 exports.create = (req, res) => {
   
-    // Create a guardian
+    // Create a roomStatus
     const roomStatus = {
       RoomStatusID: req.body.RoomStatusID,
       Status: req.body.Status
     };
   
-    // Save guardian in the database
+    // Save roomStatus in the database
     RoomStatus.create(roomStatus)
     .then(data => {
       res.send(data);
@@ -50,7 +50,7 @@ exports.create = (req, res) => {
     .catch(err => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while creating the guardianRelationship."
+          err.message || "Some error occurred while creating the roomStatusRelationship."
       });
     });
     }
@@ -58,10 +58,10 @@ exports.create = (req, res) => {
   ;
 // bulk
  exports.createmany = (req, res) => {
-    // Create a guardian
+    // Create a roomStatus
     const roomStatuses = req.body;
   
-    // Save guardian in the database
+    // Save roomStatus in the database
     RoomStatus.bulkCreate(roomStatuses)
       .then(data => {
         res.send(data);
@@ -69,7 +69,7 @@ exports.create = (req, res) => {
       .catch(err => {
         res.status(500).send({
           message:
-            err.message || "Some error occurred while creating the guardian."
+            err.message || "Some error occurred while creating the roomStatus."
         });
       });
   };
@@ -82,7 +82,7 @@ exports.create = (req, res) => {
       .catch(err => {
         res.status(500).send({
           message:
-            err.message || "Some error occurred while retrieving guardians."
+            err.message || "Some error occurred while retrieving roomStatuss."
         });
       });
   };
@@ -96,7 +96,7 @@ exports.create = (req, res) => {
       })
       .catch(err => {
         res.status(500).send({
-          message: "Error retrieving Guardian_student with id=" + id
+          message: "Error retrieving roomStatus_student with id=" + id
         });
       });
   };
@@ -105,22 +105,22 @@ exports.create = (req, res) => {
     const id = req.params.id;
   
     RoomStatus.update(req.body, {
-      where: { _id: id }
+      where: { RoomStatusID: id }
     })
       .then(num => {
         if (num == 1) {
           res.send({
-            message: "guardian was updated successfully."
+            message: "roomStatus was updated successfully."
           });
         } else {
           res.send({
-            message: `Cannot update guardian with id=${id}. Maybe guardian was not found or req.body is empty!`
+            message: `Cannot update roomStatus with id=${id}. Maybe roomStatus was not found or req.body is empty!`
           });
         }
       })
       .catch(err => {
         res.status(500).send({
-          message: "Error updating guardian with id=" + id
+          message: "Error updating roomStatus with id=" + id
         });
       });
   };
@@ -129,22 +129,22 @@ exports.create = (req, res) => {
     const id = req.params.id;
   
     RoomStatus.destroy({
-      where: { _id: id }
+      where: { RoomStatusID: id }
     })
       .then(num => {
         if (num == 1) {
           res.send({
-            message: "guardian was deleted successfully!"
+            message: "roomStatus was deleted successfully!"
           });
         } else {
           res.send({
-            message: `Cannot delete guardian with id=${id}. Maybe guardian was not found!`
+            message: `Cannot delete roomStatus with id=${id}. Maybe roomStatus was not found!`
           });
         }
       })
       .catch(err => {
         res.status(500).send({
-          message: "Could not delete guardian with id=" + id
+          message: "Could not delete roomStatus with id=" + id
         });
       });
   };
@@ -156,12 +156,12 @@ exports.create = (req, res) => {
       truncate: false
     })
       .then(nums => {
-        res.send({ message: `${nums} guardians were deleted successfully!` });
+        res.send({ message: `${nums} roomStatuss were deleted successfully!` });
       })
       .catch(err => {
         res.status(500).send({
           message:
-            err.message || "Some error occurred while removing all guardians."
+            err.message || "Some error occurred while removing all roomStatuss."
         });
       });
   };
@@ -174,7 +174,7 @@ exports.create = (req, res) => {
       .catch(err => {
         res.status(500).send({
           message:
-            err.message || "Some error occurred while retrieving guardians."
+            err.message || "Some error occurred while retrieving roomStatuss."
         });
       });
   };
