9e1053b539cfcbd687d9d62d76eb2a7fc504f7da
nickyt123 <78175735+nickyt123@users.noreply.github.com>
2022-11-07 19:53:20 -0600

    Update studentStatus.controller.js

--- backend/app/controllers/studentStatus.controller.js
+++ backend/app/controllers/studentStatus.controller.js
@@ -2,32 +2,32 @@ const db = require("../models");
 const StudentStatus  = db.studentStatus;
 const Op = db.Sequelize.Op;
 
-// Create and Save a new guardian
+// Create and Save a new studentStatus
 exports.create = (req, res) => {
   
 };
 
-// Retrieve all guardians from the database.
+// Retrieve all studentStatuss from the database.
 exports.findAll = (req, res) => {
   
 };
 
-// Find a single guardian with an id
+// Find a single studentStatus with an id
 exports.findOne = (req, res) => {
   
 };
 
-// Update a guardian by the id in the request
+// Update a studentStatus by the id in the request
 exports.update = (req, res) => {
   
 };
 
-// Delete a guardian with the specified id in the request
+// Delete a studentStatus with the specified id in the request
 exports.delete = (req, res) => {
   
 };
 
-// Delete all guardians from the database.
+// Delete all studentStatuss from the database.
 exports.deleteAll = (req, res) => {
   
 };
@@ -36,13 +36,13 @@ exports.deleteAll = (req, res) => {
 
 exports.create = (req, res) => {
   
-    // Create a guardian
+    // Create a studentStatus
     const StudentStatus = {
       StudentStatusID: req.body.StudentStatusID,
       Status: req.body.Status
     };
   
-    // Save guardian in the database
+    // Save studentStatus in the database
     StudentStatus.create(StudentStatus)
     .then(data => {
       res.send(data);
@@ -50,7 +50,7 @@ exports.create = (req, res) => {
     .catch(err => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while creating the guardianRelationship."
+          err.message || "Some error occurred while creating the studentStatusRelationship."
       });
     });
     }
@@ -58,18 +58,18 @@ exports.create = (req, res) => {
   ;
 // bulk
  exports.createmany = (req, res) => {
-    // Create a guardian
-    const guardians = req.body;
+    // Create a studentStatus
+    const studentStatuss = req.body;
   
-    // Save guardian in the database
-    StudentStatus.bulkCreate(guardians)
+    // Save studentStatus in the database
+    StudentStatus.bulkCreate(studentStatuss)
       .then(data => {
         res.send(data);
       })
       .catch(err => {
         res.status(500).send({
           message:
-            err.message || "Some error occurred while creating the guardian."
+            err.message || "Some error occurred while creating the studentStatus."
         });
       });
   };
@@ -82,7 +82,7 @@ exports.create = (req, res) => {
       .catch(err => {
         res.status(500).send({
           message:
-            err.message || "Some error occurred while retrieving guardians."
+            err.message || "Some error occurred while retrieving studentStatuss."
         });
       });
   };
@@ -96,7 +96,7 @@ exports.create = (req, res) => {
       })
       .catch(err => {
         res.status(500).send({
-          message: "Error retrieving Guardian_student with id=" + id
+          message: "Error retrieving studentStatus_student with id=" + id
         });
       });
   };
@@ -105,22 +105,22 @@ exports.create = (req, res) => {
     const id = req.params.id;
   
     StudentStatus.update(req.body, {
-      where: { _id: id }
+      where: { StudentStatusID: id }
     })
       .then(num => {
         if (num == 1) {
           res.send({
-            message: "guardian was updated successfully."
+            message: "studentStatus was updated successfully."
           });
         } else {
           res.send({
-            message: `Cannot update guardian with id=${id}. Maybe guardian was not found or req.body is empty!`
+            message: `Cannot update studentStatus with id=${id}. Maybe studentStatus was not found or req.body is empty!`
           });
         }
       })
       .catch(err => {
         res.status(500).send({
-          message: "Error updating guardian with id=" + id
+          message: "Error updating studentStatus with id=" + id
         });
       });
   };
@@ -129,22 +129,22 @@ exports.create = (req, res) => {
     const id = req.params.id;
   
     StudentStatus.destroy({
-      where: { _id: id }
+      where: { StudentStatusID: id }
     })
       .then(num => {
         if (num == 1) {
           res.send({
-            message: "guardian was deleted successfully!"
+            message: "studentStatus was deleted successfully!"
           });
         } else {
           res.send({
-            message: `Cannot delete guardian with id=${id}. Maybe guardian was not found!`
+            message: `Cannot delete studentStatus with id=${id}. Maybe studentStatus was not found!`
           });
         }
       })
       .catch(err => {
         res.status(500).send({
-          message: "Could not delete guardian with id=" + id
+          message: "Could not delete studentStatus with id=" + id
         });
       });
   };
@@ -156,12 +156,12 @@ exports.create = (req, res) => {
       truncate: false
     })
       .then(nums => {
-        res.send({ message: `${nums} guardians were deleted successfully!` });
+        res.send({ message: `${nums} studentStatuss were deleted successfully!` });
       })
       .catch(err => {
         res.status(500).send({
           message:
-            err.message || "Some error occurred while removing all guardians."
+            err.message || "Some error occurred while removing all studentStatuss."
         });
       });
   };
@@ -174,7 +174,7 @@ exports.create = (req, res) => {
       .catch(err => {
         res.status(500).send({
           message:
-            err.message || "Some error occurred while retrieving guardians."
+            err.message || "Some error occurred while retrieving studentStatuss."
         });
       });
   };
