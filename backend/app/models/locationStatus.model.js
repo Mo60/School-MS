70cb6e80818c3127887df544c35c3f9c032cff8a
nickyt123 <78175735+nickyt123@users.noreply.github.com>
2022-11-12 00:37:33 -0600

    Update locationStatus.model.js

--- backend/app/models/locationStatus.model.js
+++ backend/app/models/locationStatus.model.js
@@ -8,6 +8,10 @@ module.exports = (sequelize, Sequelize) => {
     Status: {
       type: Sequelize.STRING
     },
+    IsDeleted: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: 0,
+      }
   },
                            
                                
