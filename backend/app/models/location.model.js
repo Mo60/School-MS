c065ca7bf17b3e648bdb61a719a92b79ce05c358
nickyt123 <78175735+nickyt123@users.noreply.github.com>
2022-11-12 00:37:14 -0600

    Update location.model.js

--- backend/app/models/location.model.js
+++ backend/app/models/location.model.js
@@ -15,6 +15,10 @@ module.exports = (sequelize, Sequelize) => {
           key: 'LocationStatusID', 
         }
     },
+    IsDeleted: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: 0,
+      }
   }, 
                            
                                
