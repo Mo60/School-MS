d6ef1c79cf97393a47e9b60de7944d2f0f2366b2
Mo Khalil <51033011+Mo60@users.noreply.github.com>
2023-07-19 02:54:58 +0000

    added school MS message

--- backend/server.js
+++ backend/server.js
@@ -77,7 +77,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to school MS application." });
 });
 
 require("./app/routes/turorial.routes")(app);
